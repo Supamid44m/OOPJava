b4c27c69836c4f0be9013b7cc7573d417082b69b
supamid.ak.63@ubu.ac.th <supamid.ak.63@ubu.ac.th>
2022-11-07 20:24:34 +0700

    change

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
      public static void main(String[] args) {
-        
+        openwheelcar car1=new openwheelcar();
+        car1.brand="Redbull";
+        car1.setEngine("RB16");
+        car1.setTires(18);
+        car1.setTopSpeed(397);
+        car1.setCylinders(6);
+        car1.showInfo();
     }
 }
--- openwheelcar.java
+++ openwheelcar.java
@@ -1,8 +1,32 @@
 class openwheelcar{
     //Attribute
+    public String brand;
     private  String engine;
     private  int tires_size;
     private  int top_speed;
     private  int cylinders;
-    public   String type;
+    
+    //metthod
+    public void setEngine(String engine){
+        this.engine=engine;
+        
+    }
+    public void setTires(int tires_size){
+        this.tires_size=tires_size;
+    }
+    public void setTopSpeed(int tspeed){
+        this.top_speed=tspeed;
+    }
+    public void setCylinders(int cylinders){
+        this.cylinders=cylinders;
+    }
+
+    public void  showInfo(){
+        System.out.println("Brand:"+brand);
+        System.out.println("Engine:"+this.engine);
+        System.out.println("Tires_Size:"+this.tires_size);
+        System.out.println("TopSpeed:"+this.top_speed);
+        System.out.println("Cylinders:"+this.cylinders);
+    }
+    
 }
